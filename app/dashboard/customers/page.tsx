55618d48fd8d0ac13b0aa3e8d76daef91796afc5
Rizky1911 <rizky.rama1911@gmail.com>
2024-01-15 13:35:02 +0700

    add routing

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CustomersTable from '@/app/customers/table'
+import { customers } from '@/app/lib/placeholder-data'
+
+const customer = customers;
+
+const page = () => {
+  return (
+    <div>
+      <p>Customers Page</p>
+      <CustomersTable customers={customer}/>
+    </div>
+  )
+}
+
+export default page
