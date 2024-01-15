856ffccab26244754ac4de876be0b7586a4287b3
Rizky1911 <rizky.rama1911@gmail.com>
2024-01-15 15:49:34 +0700

    Add CRUD Actions

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -68,21 +68,6 @@ export default function NavLinks() {
             </li>
             <li>
               <Link
-                href={'/dashboard/customers'}
-                onClick={() => {}}
-                className={clsx(
-                  'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-                  {
-                    'bg-sky-100 text-blue-600': pathname === '/dashboard/customers',
-                  },
-                )}
-              >
-                <UserGroupIcon className="w-6"/>
-                <p className="hidden md:block">{'Customers'}</p>
-              </Link>
-            </li>
-            <li>
-              <Link
                 href={'/dashboard/invoices'}
                 onClick={() => {}}
                 className={clsx(
@@ -94,6 +79,21 @@ export default function NavLinks() {
               >
                 <DocumentDuplicateIcon className="w-6"/>
                 <p className="hidden md:block">{'Invoices'}</p>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={'/dashboard/customers'}
+                onClick={() => {}}
+                className={clsx(
+                  'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+                  {
+                    'bg-sky-100 text-blue-600': pathname === '/dashboard/customers',
+                  },
+                )}
+              >
+                <UserGroupIcon className="w-6"/>
+                <p className="hidden md:block">{'Customers'}</p>
               </Link>
             </li>
       </ul>
